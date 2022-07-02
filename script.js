83081bfc7bbe204f2ef5c3a8db9ecce39c47a735
hannesxc <108219142+hannesxc@users.noreply.github.com>
2022-07-02 20:48:19 +0530

    final stages

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+// Pre setting values to avoid typeError
+
+var principal = 0;
+var rate = 15;
+var years = 1;
+var interest = 0;
+var year = 1;
+var amount = 0;
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    year = new Date().getFullYear()+parseInt(years);
+    interest = (principal * years * rate)/100;
+    amount = interest + parseFloat(principal);
+    if (principal > 0) 
+    {
+        document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>,\<br\>at an interest rate of \<mark>"+rate+"\</mark>%\<br\>You will receive an amount of \<mark>"+amount+"\</mark>,\<br\>in the year \<mark>"+year+"\</mark>,\<br\>";
+    }
+    // Alert if principal < 0
+    else
+    {
+        alert("Please enter a positive number: ");
+        document.getElementById("principal").select();
+    }
 }
-        
+
+function currentRate()
+{
+    var slider = document.getElementById("rate");
+    var currentpos = document.getElementById("display_rate");
+    currentpos.innerHTML = slider.value +"%";
+}
